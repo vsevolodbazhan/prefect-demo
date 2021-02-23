c4cd0bc3120f614c01af03fcaf02c402049cbb62
Vsevolod Bazhan <vsevozhan@yandex.ru>
2021-02-23 17:05:55 +1000

    Add `transform_vaccination_data` task to `vaccination` flow

--- vaccinations.py
+++ vaccinations.py
@@ -5,9 +5,7 @@ import requests
 from prefect import Flow, Parameter, task
 from prefect.engine.results import LocalResult
 
-VACCINATIONS_DATA_URL: str = (
-    'https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv'
-)
+VACCINATIONS_DATA_URL = 'https://github.com/owid/covid-19-data/raw/master/public/data/vaccinations/vaccinations.csv'
 
 
 @task
@@ -17,22 +15,28 @@ def extract_raw_vaccinations_data(vaccinations_data_url: str) -> pandas.DataFram
 
 
 @task
-def clean_vaccination_data(raw_vaccinations_data: pandas.DataFrame) -> pandas.DataFrame:
-    vaccination_data = raw_vaccinations_data[['location', 'date', 'total_vaccinations']]
-    vaccination_data.dropna(inplace=True)
-    return vaccination_data.astype({'location': str, 'date': str, 'total_vaccinations': int})
+def clean_vaccinations_data(raw_vaccinations_data: pandas.DataFrame) -> pandas.DataFrame:
+    vaccinations_data = raw_vaccinations_data[['location', 'date', 'total_vaccinations']]
+    vaccinations_data.dropna(inplace=True)
+    return vaccinations_data.astype({'location': str, 'date': str, 'total_vaccinations': int})
+
+
+@task
+def transform_vaccination_data(clean_vaccinations_data: pandas.DataFrame) -> pandas.DataFrame:
+    return clean_vaccinations_data.groupby(by=['location', 'date']).sum()
 
 
 @task(result=LocalResult(dir='./vaccinations', location='{today}'))
-def save_vaccinations_data(clean_vaccinations_data: pandas.DataFrame) -> str:
-    return clean_vaccinations_data.to_csv(index=False)
+def save_vaccinations_data(transformed_vaccination_data: pandas.DataFrame) -> str:
+    return transformed_vaccination_data.to_csv()
 
 
 with Flow('vaccinations') as flow:
-    vaccinations_data_url = Parameter('vaccination_data_url', default=VACCINATIONS_DATA_URL)
+    vaccinations_data_url = Parameter('vaccinations_data_url', default=VACCINATIONS_DATA_URL)
 
     raw_vaccinations_data = extract_raw_vaccinations_data(vaccinations_data_url=vaccinations_data_url)
-    clean_vaccination_data = clean_vaccination_data(raw_vaccinations_data=raw_vaccinations_data)
-    save_vaccinations_data(clean_vaccinations_data=clean_vaccination_data)
+    clean_vaccinations_data = clean_vaccinations_data(raw_vaccinations_data=raw_vaccinations_data)
+    transformed_vaccination_data = transform_vaccination_data(clean_vaccinations_data=clean_vaccinations_data)
+    save_vaccinations_data(transformed_vaccination_data=transformed_vaccination_data)
 
 flow.register(project_name='covid')
